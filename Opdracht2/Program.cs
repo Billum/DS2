442c2334b86c58ab763059c9481a2121e3df4e8d
Billum <tomas.billum@outlook.com>
2015-05-01 11:10:10 +0200

    done

--- Opdracht2/Program.cs
+++ Opdracht2/Program.cs
@@ -8,98 +8,71 @@ namespace Opdracht2
         {
             string s = Console.ReadLine();
             string[] input = s.Split(' ');
-
-            long[] alfa = new long[] {14, 1, 0, 22, 23, 24, 10000};
             long x = Int64.Parse(input[0]);
             long beta = Int64.Parse(input[1]);
             int gamma = int.Parse(input[2]);
 
-            //for (int i = 0; i < alfa.Length; i++)
-            //{
-            //    alfa[i] = Int64.Parse(Console.ReadLine());
-            //}
-
             Levels[] levels = makeLevels(x, beta, gamma);
 
-            for (int i = 0; i < alfa.Length; i++)
+            string alpha = Console.ReadLine();
+
+            while( alpha != null)
             {
-                findLevel(levels, alfa[i], gamma);
+                Console.WriteLine(binsearch(levels, Int64.Parse(alpha), 0, levels.Length - 1));
+                alpha = Console.ReadLine();
             }
         }
-        static Levels[] makeLevels(long x, long beta, int gamma)
+        public static Levels[] makeLevels(long x, long beta, int gamma)
         {
             Levels[] level = new Levels[gamma];
-            level[0] = new Levels(0, (int)x - 1);
-            int i = 0;
+            level[0] = new Levels(0, x);
 
-            while (i < gamma - 1)
+            for (int i = 1; i < gamma; i++)
             {
-                double c = ceiling((double)(x) / (double)(beta));
+                long c = ceiling(x, beta);
 
-                level[i + 1] = new Levels((int)x, (int)(x + c) - 1);
-                x = (int)(x + c);
-                i++;
+                level[i] = new Levels(x, x + c);
+                x = x + c;
             }
             return level;
         }
-        static void findLevel(Levels[] levels, long alfa, long gamma)
+        public static long ceiling(long x, long beta)
         {
-            for (int i = 0; i < gamma; i++)
-            {
-                if (alfa >= levels[i].begin && alfa <= levels[i].eind)
-                {
-                    Console.WriteLine(i + 1);
-                    break;
-                }
-                else if (alfa > levels[gamma - 1].eind)
-                {
-                    Console.WriteLine(gamma);
-                    break;
-                }
-            }
-        }
-        public static int ceiling(double x)
-        {
-            if (x != (int)x)
-            {
-                string y = x.ToString();
-                string[] split = y.Split('.');
-
-                long integr = Int64.Parse(split[0]);
-                long deciml = Int64.Parse(split[1]);
-
-                if (deciml != 0)
-                    return (int)integr + 1;
-                else
-                    return (int)integr;
-            }
+            if (x % beta == 0)
+                return x / beta;
             else
-                return (int)x;
+                return (x / beta) + 1;
         }
-        public static int binsearch(int[] A, int key, int min, int max)
+        public static int binsearch(Levels[] A, long key, int min, int max)
         {
             if (max < min)
-                return -1;
+                return A.Length;         
             else
             {
                 int mid = (min + max) / 2;
-                if (A[mid] > key)
+
+                if (A[mid].begin <= key && key < A[mid].eind)
+                    return mid + 1;
+                else if (A[mid].begin > key)
                     return binsearch(A, key, min, mid - 1);
-                else if (A[mid] < key)
+                else if (A[mid].eind <= key)
                     return binsearch(A, key, mid + 1, max);
                 else
-                    return mid;
+                    return -1;
             }
         }
     }
     class Levels
     {
-        public int begin;
-        public int eind;
-        public Levels(int begin, int eind)
+        public long begin;
+        public long eind;
+        public Levels(long begin, long eind)
         {
             this.begin = begin;
             this.eind = eind;
         }
     }
 }
+
+// Tomas Billum
+// 4161882
